65b854e2e4551b87b46ef9ad67a080772367fdda
River-Xu <872534428@qq.com>
2017-09-28 15:42:52 +1000

    Add files via upload

---
+++ 2048/2048-current/myjs/class.js
@@ -0,0 +1,25 @@
+class Cell{
+    constructor(x,y,value){
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+    getX(){
+        return this.x;
+    }
+    setX(x){
+        this.x = x;
+    }
+    getY(){
+        return this.y;
+    }
+    setY(y){
+        this.y = y;
+    }
+    getValue(){
+        return this.value;
+    }
+    setValue(value){
+        this.value = value;
+    }
+}
